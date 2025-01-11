920e08c3cdc0348b1e73a0b970853134f76fd2f4
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-01-11 15:02:19 +0530

    middleware

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+export const publicRoutes = [
+    "/"
+]
+
+export const authRoutes = [
+    "/login",
+    "/signup",
+    "/reset"
+]
+
+export const apiPrefix = "/api/auth"
+
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard"
