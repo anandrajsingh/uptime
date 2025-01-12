fe32e7b44857caf04c049371a56a78191b3b4ac3
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-01-12 21:37:01 +0530

    login bug

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -7,31 +7,35 @@ import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
 import { AuthError } from "next-auth";
 import { z } from "zod";
+import bcrypt from "bcryptjs"
 
-export const login = async(values: z.infer<typeof LoginSchema>) => {
+export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values)
 
-    if(!validatedFields.success) return {error: "Invalid Fields"}
+    if (!validatedFields.success) return { error: "Invalid Fields" }
 
     const { email, password } = validatedFields.data;
 
     const existingUser = await getUserByEmail(email)
 
-    if(!existingUser || !existingUser.email || !existingUser.password){
-        return { error: "Email does not exist"}
+    if (!existingUser || !existingUser.email || !existingUser.password) {
+        return { error: "Email does not exist" }
     }
 
-    if(!existingUser.emailVerified){
+    const correctPassword = await bcrypt.compare(password, existingUser.password)
+    if (!correctPassword) return { error: "Password did not match" }
+
+    if (!existingUser.emailVerified) {
         const verificationToken = await generateVerificationToken(existingUser.email);
 
         await sendVerificationEmail(
             verificationToken.email,
             verificationToken.token
         )
-        
-        return { success: "Confirmation email sent"}
+
+        return { success: "Confirmation email sent" }
     }
-    
+
 
     try {
         await signIn("credentials", {
@@ -40,12 +44,12 @@ export const login = async(values: z.infer<typeof LoginSchema>) => {
             redirectTo: DEFAULT_LOGIN_REDIRECT
         })
     } catch (error) {
-        if(error instanceof AuthError){
-            switch (error.type){
+        if (error instanceof AuthError) {
+            switch (error.type) {
                 case "CredentialsSignin":
-                    return { error : "Invalid Credentials"}
+                    return { error: "Invalid Credentials" }
                 default:
-                    return { error : "Something went wrong"}
+                    return { error: "Something went wrong" }
             }
         }
         throw error
