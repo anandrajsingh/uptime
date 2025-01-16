c321cf91a953afab41274b2d8137ee4e11f82f46
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-01-16 17:37:22 +0530

    incidents page

--- src/app/dashboard/incidents/page.tsx
+++ src/app/dashboard/incidents/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 export default function Monitors() {
     return (
-        <div>
-            Incidents
+        <div className="h-screen w-full flex items-center justify-center">
+            <div >
+                <div className="font-bold">
+                    We create incident everytime your monitor fails
+                </div>
+                <Button className="font-bold">
+                    Report new incident
+                </Button>
+            </div>
         </div>
     )
 }
