73e840c23379a304e191eba09f59d84c6b96e4fa
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-04-01 17:34:02 +0530

    analytics page

---
+++ src/app/(authenticated)/analytics/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Monitors() {
+    return (
+        <div className="h-screen w-full flex items-center justify-center">
+            <div >
+                <div className="font-bold">
+                    Ananlytics Page
+                </div>
+            </div>
+        </div>
+    )
+}
