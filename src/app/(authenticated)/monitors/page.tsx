6eca39e2ab3c967d49992ac2aa9703c9d0ad1ed2
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-04-02 12:11:21 +0530

    server side render /monitors route

--- src/app/(authenticated)/monitors/page.tsx
+++ src/app/(authenticated)/monitors/page.tsx
@@ -1,25 +1,23 @@
-"use client"
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
-import { Circle, MoreVertical} from "lucide-react";
+import { db } from "@/lib/db";
+import { Circle, MoreVertical } from "lucide-react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
-type Monitor = {
-    id: string,
-    url: string
-}
 
-export default function Monitors() {
-    const [monitors, setMonitors] = useState<Monitor[]>([])
+export default async function Monitors() {
+    
+    const session = await auth()
+    if (!session || !session.user?.id) {
+        return { error: 'Unauthorized' };
+    }
 
-    useEffect(() => {
-        fetch("/api/monitor")
-          .then((res) => res.json())
-          .then((data) => {
-            setMonitors(data);
-          });
-      }, []);
-      
+    const monitors = await db.monitor.findMany({
+        where: {
+            userId: session.user.id
+        }
+    })
+
     return (
         <div className="h-screen w-full flex items-center justify-center">
             {monitors.length === 0 ? (
@@ -40,24 +38,24 @@ export default function Monitors() {
                     <ul className="space-y-2">
                         {monitors.map((monitor) => (
                             <li key={monitor.id} className="flex justify-between items-center p-4 bg-[#2A2A3A] rounded-lg">
-                            
-                            <div className="flex items-center gap-3">
-                                <Circle className={`h-4 w-4 text-green-400`} />
-                                <Link href={`/monitors/${monitor.id}`} className="text-white font-semibold hover:underline">
-                                    {monitor.url}
-                                </Link>
-                                <span className={`text-sm text-green-300`}>
-                                    Up
-                                </span>
-                            </div>
 
-                            
-                            <div className="flex items-center gap-4">
-                            
-                                <span className="text-gray-400 text-sm">10 min ago</span>
-                                <MoreVertical className="text-gray-400 cursor-pointer" />
-                            </div>
-                        </li>
+                                <div className="flex items-center gap-3">
+                                    <Circle className={`h-4 w-4 text-green-400`} />
+                                    <Link href={`/monitors/${monitor.id}`} className="text-white font-semibold hover:underline">
+                                        {monitor.url}
+                                    </Link>
+                                    <span className={`text-sm text-green-300`}>
+                                        Up
+                                    </span>
+                                </div>
+
+
+                                <div className="flex items-center gap-4">
+
+                                    <span className="text-gray-400 text-sm">10 min ago</span>
+                                    <MoreVertical className="text-gray-400 cursor-pointer" />
+                                </div>
+                            </li>
                         ))}
                     </ul>
                     <Link href="/monitors/new">
