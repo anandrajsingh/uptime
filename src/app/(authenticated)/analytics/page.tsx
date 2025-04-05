fc54e21b25cf7a4180880647a1f90d59c8370193
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-04-05 19:17:27 +0530

    analytics page

--- src/app/(authenticated)/analytics/page.tsx
+++ src/app/(authenticated)/analytics/page.tsx
@@ -1,13 +1,59 @@
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { Circle, MoreVertical } from "lucide-react";
+import Link from "next/link";
 
+export default async function Analytics() {
 
-export default function Monitors() {
+    const session = await auth()
+    if (!session || !session.user?.id) {
+        return { error: 'Unauthorized' };
+    }
+    const projectList = await db.project.findMany({
+        where: {
+            userId: session.user.id
+        },
+    })
+
+    const formatUrl = (url?: string): string => {
+        if (!url) return '';
+        return url.replace(/^(https?:\/\/)/, '');
+    };
+
     return (
         <div className="h-screen w-full flex items-center justify-center">
-            <div >
-                <div className="font-bold">
-                    Ananlytics Page
+            {projectList.length === 0 ? (
+                <div>
+                    <div className="font-bold py-2">Create your first analytics project</div>
+                    <Link href="/analytics/new">
+                        <Button size="sm" className="font-bold">Create Project</Button>
+                    </Link>
                 </div>
-            </div>
+            ) : (
+                <div className="w-full max-w-lg space-y-4">
+                    <h2 className="font-bold text-xl">Your Analytics Project</h2>
+                    <ul className="space-y-2">
+                        {projectList.map((project) => (
+                            <li key={project.id} className="flex justify-between items-center p-4 bg-[#2A2A3A] rouded-lg">
+                                <div className="flex items-center gap-3">
+                                    <Circle className="h-2 w-2" />
+                                    <Link href={`/analytics/${project.id}`} className="text-white font-semibold hover:underline">
+                                        {formatUrl(project.url)}
+                                    </Link>
+                                </div>
+
+                                <div className="flex items-center gap-4">
+                                    <MoreVertical className="text-gray-400 cursor-pointer" />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    <Link href="/analytics/new">
+                        <Button size="sm" className="font-bold my-4">Add new Project</Button>
+                    </Link>
+                </div>
+            )}
         </div>
     )
 }
