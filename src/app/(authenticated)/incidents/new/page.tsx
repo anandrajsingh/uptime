917b06c048a69b28cbe825d26a69d8725e93c6f8
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-04-05 17:54:56 +0530

    report incident page

---
+++ src/app/(authenticated)/incidents/new/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function NewIncident() {
+    return (
+        <div className="h-screen w-full flex items-center justify-center">
+            <div >
+                <div className="font-bold">
+                    Coming soon
+                </div>
+            </div>
+        </div>
+    )
+}
